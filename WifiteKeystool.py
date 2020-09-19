16929c837809a01339165c0482c62b03b44a340d
lambsouls <70055403+lambsouls@users.noreply.github.com>
2020-09-20 06:10:22 +0800

    Add files via upload

--- WifiteKeystool.py
+++ WifiteKeystool.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[35]:
+# In[2]:
 
 
 #全局引用
@@ -10,13 +10,13 @@ import random
 x=0
 
 
-# In[43]:
+# In[3]:
 
 
 #函数区域 ↓
 
 
-# In[24]:
+# In[4]:
 
 
 def rounum(a,b):
@@ -38,13 +38,13 @@ def rouda():
     return num    
 
 
-# In[ ]:
+# In[5]:
 
 
 #主体
 
 
-# In[ ]:
+# In[6]:
 
 
 #输出标题
@@ -53,7 +53,7 @@ while a1>0:
     print(end='—')
     a1-=1
 print("")
-print("* Wifite Keystool v1.00 *")
+print("* Wifite Keystool v1.02 *")
 a2=13
 while a2>0:
     print(end='—')
@@ -64,100 +64,102 @@ print("")
 # In[ ]:
 
 
+keys = []
+print("正在读取预备处理文件...")
+f = open('wordlist-probable.txt',encoding='utf-8')
+line = f.readline() # 读取第一行
+while line:
+    line=line.strip('\n')
+    txt_data = str(line) # 可将字符串变为元组
+    keys.append(txt_data) # 列表增加
+    line = f.readline() # 读取下一行
+print("读取完毕")
+print('')
+
 #输入区域
 #输入最小位数
-print("Minimum number of digits:")
+print(end="输入最小位数(不小于6位):")
 minimun=input()
-while minimun.isdigit()==0: #判断是否输入的是数字
-    print("[Warning] Illegal input,please re-enter:")
-    minimun=input()
-while int(minimun)<6:#判断是否大于6位
-    print("[Warning] Illegal input,please re-enter:")
-    minimun=input()
-while int(minimun)>20:#判断是否小于20位
-    print("[Warning] Illegal input,please re-enter:")
-    minimun=input()
+p=0
+while p==0:
+    if minimun.isdigit(): #判断是否输入的是数字
+        p+=1
+        if int(minimun)>=6:#判断是否大于6位
+            p+=1
+            if int(minimun)<=20:#判断是否小于20位
+                p+=1
+    if p!=3:
+        p=0
+        print(end="[Warning] 非法输入，请重新输入最小位数(不小于6位):")
+        minimun=input()
 print("")
 
-
 #输入最大位数
-print("Maximum number of digits:")
+print(end="输入最大位数(不大于20位):")
 maximun=input()
-while maximun.isdigit()==0: #判断是否输入的是数字
-    print("[Warning] Illegal input,please re-enter:")
-    maximun=input()
-while int(minimun)>int(maximun): #判断是否大于最小位数
-    print("[Warning] Illegal input,please re-enter:")
-    maximun=input()
-while int(maximun)>20: #判断是否小于20位
-    print("[Warning] Illegal input,please re-enter:")
-    maximun=input()
+p=0
+while p==0:
+    if maximun.isdigit(): #判断是否输入的是数字
+        p+=1
+        if int(minimun)<=int(maximun): #判断是否大于最小位数
+            p+=1
+            if int(maximun)<=20: #判断是否小于20位
+                p+=1
+    if p!=3:
+        p=0
+        print(end="[Warning] 非法输入，请重新输入最大位数(不大于20位):")
+        maximun=input()
 print("")
 
 #是否包含数字
-print("Does the keys contain numbers?")
-print("[0] NO")
-print("[1] YES")
+print(end="是否包含数字([0]NO [1]YES):")
 havenumbers=input()
-while havenumbers.isdigit()==0: #判断是否输入的是数字
-    print("[Warning] Illegal input,please re-enter:")
-    print("[0] NO")
-    print("[1] YES")
-    havenumbers=input()
-while int(havenumbers)>1: #判断是否小于等于1
-    print("[Warning] Illegal input,please re-enter:")
-    print("[0] NO")
-    print("[1] YES")
-    havenumbers=input()
-while int(havenumbers)<0: #判断是否大于等于0
-    print("[Warning] Illegal input,please re-enter:")
-    print("[0] NO")
-    print("[1] YES")
-    havenumbers=input()
+p=0
+while p==0:
+    if havenumbers.isdigit(): #判断是否输入的是数字
+        p+=1
+        if int(havenumbers)<=1: #判断是否小于等于1
+            p+=1
+            if int(havenumbers)>=0: #判断是否大于等于0
+                p+=1
+    if p!=3:
+        p=0
+        print(end="[Warning] 非法输入，请重新输入是否包含数字([0]NO [1]YES):")
+        havenumbers=input()
 print("")
 
 #是否包含小写字母
-print("Does the keys contain lowercase letters?")
-print("[0] NO")
-print("[1] YES")
+print(end="是否包含小写字母([0]NO [1]YES):")
 havelowercaseletters=input()
-while havelowercaseletters.isdigit()==0: #判断是否输入的是数字
-    print("[Warning] Illegal input,please re-enter:")
-    print("[0] NO")
-    print("[1] YES")
-    havelowercaseletters=input()
-while int(havelowercaseletters)>1: #判断是否小于等于1
-    print("[Warning] Illegal input,please re-enter:")
-    print("[0] NO")
-    print("[1] YES")
-    havelowercaseletters=input()
-while int(havelowercaseletters)<0: #判断是否大于等于0
-    print("[Warning] Illegal input,please re-enter:")
-    print("[0] NO")
-    print("[1] YES")
-    havelowercaseletters=input()
+p=0
+while p==0:
+    if havelowercaseletters.isdigit(): #判断是否输入的是数字
+        p+=1
+        if int(havelowercaseletters)<=1: #判断是否小于等于1
+            p+=1
+            if int(havelowercaseletters)>=0: #判断是否大于等于0
+                p+=1
+    if p!=3:
+        p=0
+        print(end="[Warning] 非法输入，请重新输入是否包含小写字母([0]NO [1]YES):")
+        havelowercaseletters=input()
 print("")
 
 #是否包含大写字母
-print("Does the keys contain lowercase capital letters?")
-print("[0] NO")
-print("[1] YES")
+print(end="是否包含大小字母([0]NO [1]YES):")
 havecapitalletters=input()
-while havecapitalletters.isdigit()==0: #判断是否输入的是数字
-    print("[Warning] Illegal input,please re-enter:")
-    print("[0] NO")
-    print("[1] YES")
-    havecapitalletters=input()
-while int(havecapitalletters)>1: #判断是否小于等于1
-    print("[Warning] Illegal input,please re-enter:")
-    print("[0] NO")
-    print("[1] YES")
-    havecapitalletters=input()
-while int(havecapitalletters)<0: #判断是否大于等于0
-    print("[Warning] Illegal input,please re-enter:")
-    print("[0] NO")
-    print("[1] YES")
-    havecapitalletters=input()
+p=0
+while p==0:
+    if havecapitalletters.isdigit(): #判断是否输入的是数字
+        p+=1
+        if int(havecapitalletters)<=1: #判断是否小于等于1
+            p+=1
+            if int(havecapitalletters)>=0: #判断是否大于等于0
+                p+=1
+    if p!=3:
+        p=0
+        print(end="[Warning] 非法输入，请重新输入是否包含大小字母([0]NO [1]YES):")
+        havecapitalletters=input()
 print("")
 
 #把参数变成整型
@@ -209,8 +211,10 @@ while x==0:
             nx+=str(rouxiao())
         if n1==3:
             nx+=str(rouda()) 
-    print(nx)
-    f = open('wordlist-probable.txt', 'a')
-    f.write(nx)
-    f.write('\n')
+    if nx not in keys:
+        keys.append(nx)
+        print(nx)
+        f = open('wordlist-probable.txt', 'a')
+        f.write(nx)
+        f.write('\n')
 
